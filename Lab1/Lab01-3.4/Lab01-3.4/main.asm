1933727bc7599c7b2e35a99844204ff35461fc50
balabi <42229593+balabi@users.noreply.github.com>
2018-08-14 20:43:01 +1000

    lab 1, 3.4 done

---
+++ Lab1/Lab01-3.4/Lab01-3.4/main.asm
@@ -0,0 +1,137 @@
+; Lab 01, Task 3.4 
+;
+; Author: Ihor Balaban
+; zID: z5229133
+; Date: 14/8/2018
+; Version: .1
+
+.include "m2560def.inc" 
+
+.def char_i = r16
+.def char_j = r17
+.def char_k = r18	; define 3 registers for storing i, j, k
+.def C_byte0 = r7
+.def C_byte1 = r8	; r8:r7 used to store  C += A*B
+.def a_ij = r19		; used to store a[i][j] temporarily
+.def b_ij = r20		; used to store b[i][j] temporarily 
+.def temp = r24		; used to help with moving through the array adresses 
+
+.equ n_max = 5		; defines the max size of the arrays
+
+.dseg				; define data segment
+.org 0x200			; set starting address
+A_array: .byte 25	; allocate 25 bytes for array A 
+B_array: .byte 25	; allocate 25 bytes for array B
+C_array: .byte 50	; allocate 50 bytes for array C 
+
+.cseg
+.org 0x100			; set the origion of the code to 0x100
+
+; initialize A,B,C
+ldi xl, low(A_array)
+ldi xh, high(A_array)	; set x to point to array A
+ldi yl, low(B_array)
+ldi yh, high(B_array)	; set y to point to array B
+ldi zl, low(C_array)
+ldi zh, high(C_array)	; set z to point to array C
+
+clr char_i		; i = 0
+main_loop:
+	clr char_j	; j = 0 
+loop_1:
+	; store A[i][j]
+	mov a_ij, char_i
+	add a_ij, char_j	; a[i][j] = i + j
+	st x+, a_ij			; store in data memory 
+		
+	; store B[i][j]
+	mov b_ij, char_i
+	sub b_ij, char_j	; B[i][j] = i - j 
+	st y+, b_ij			; store in data memory 
+
+	; store C[i][j] 
+	clr C_byte0
+	clr C_byte1	; C[i][j] = 0
+	st z+, C_byte0
+	st z+, C_byte1	; store C[i][j] in little endian order
+
+	inc char_j		; j++
+	cpi char_j, n_max
+	brlt loop_1		; branches to inner loop 	
+
+	inc char_i		; i++
+	cpi char_i, n_max
+	brlt main_loop	; branches to outer loop 
+
+	; reinitialize array pointers 
+	ldi xl, low(A_array)
+	ldi xh, high(A_array)	; set x to point to array A
+	ldi zl, low(C_array)
+	ldi zh, high(C_array)	; set z to point to array C
+
+	clr char_i		; i = 0
+second_loop:
+	clr char_j		; j = 0
+	
+	ldi yl, low(B_array)
+	ldi yh, high(B_array)	; reset y to point to start of B array
+
+loop_2:
+	clr char_k		; k = 0
+	clr C_byte0
+	clr C_byte1		; reset running sum of C 
+
+loop_3:
+	ld a_ij, x+	; load A[i][k] and increment x
+	ld b_ij, y+	; load B[i][k] and increment y
+
+	mul a_ij, b_ij	; multiply a[i][k] * b[k][j]
+	mov r21, r0
+	clr r22			; used for sign extension
+	cp r21, r22	
+	brlt negative	
+	rjmp C_p_e
+
+negative:			; used for sign extension on negative numbers 
+	ser r22
+	
+C_p_e:				; calculates the running sum
+	add C_byte0, r21
+	adc C_byte1, r22
+
+	; Properly increment B 
+	ldi temp, 4		; temp variable equals 4
+	clr r22			; use for sign extension 
+	add yl, temp
+	adc yh, r22		; move y pointer to next position 
+
+	inc char_k
+	cpi char_k, n_max
+	brlt loop_3
+
+	; store C[i][j]
+	st z+, C_byte0
+	st z+, C_byte1
+
+	ldi temp, 24
+	sub yl, temp
+	sbc yh, r22		; set the pointer to point to next column in array B
+
+	ldi temp, 5
+	sub xl, temp
+	sbc xh, r22		; move y pointer to previous row 
+
+	inc char_j
+	cpi char_j, n_max
+	brlt loop_2
+
+	ldi temp, 5
+	add xl, temp
+	adc xh, r22		; move x pointer to point to next row in A 
+
+	inc char_i
+	cpi char_i, n_max
+	brlt second_loop
+	
+loop:
+	rjmp loop
